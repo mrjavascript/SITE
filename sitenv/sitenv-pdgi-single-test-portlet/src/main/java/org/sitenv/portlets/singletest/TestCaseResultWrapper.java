eb41dd128a64c574d9d898ea7d7b414210184f2c
tlahoda <tlahoda@gmail.com>
2013-12-28 14:16:31 -0700

    A wrapper for test case results

---
+++ sitenv/sitenv-pdgi-single-test-portlet/src/main/java/org/sitenv/portlets/singletest/TestCaseResultWrapper.java
@@ -0,0 +1,40 @@
+package org.sitenv.portlets.singletest;
+
+public class TestCaseResultWrapper {
+    private String name;
+    private String status;
+    private String request;
+	private String response;
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+    public String getRequest() {
+		return request;
+	}
+	
+	public void setRequest(String request) {
+		this.request = request;
+	}
+	
+	public String getResponse() {
+		return response;
+	}
+	
+	public void setResponse(String response) {
+		this.response = response;
+	}
+}
