3b9429bcbe4bf8cb261fe3e4d3320eb63c459761
chris-watson <cxwatson@gmail.com>
2014-10-13 13:47:17 -0400

    Removed unnecessary imports

--- site-services/ccda/src/main/java/org/sitenv/services/ccda/CCDAService.java
+++ site-services/ccda/src/main/java/org/sitenv/services/ccda/CCDAService.java
@@ -1,7 +1,6 @@
 package org.sitenv.services.ccda;
 
 import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
-import org.sitenv.common.statistics.manager.StatisticsManager;
 
 
 public interface CCDAService {
